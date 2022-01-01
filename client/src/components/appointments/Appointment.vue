073c54f34b831baf7f4fce7828ea7c489858717c
Thenukad Dinu <thenukadinu@gmail.com>
2022-01-01 16:19:40 +0530

    appointment index view.

---
+++ client/src/components/appointments/Appointment.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-card v-if="appointment" width="400" class="mx-4 appointment pointer">
+    <v-img
+      height="120px"
+      src="https://images.pexels.com/photos/1257894/pexels-photo-1257894.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    >
+      <v-card-title class="white--text mt-8">
+        <p class="ml-3">Appoinment {{ appointment.appoinmentDateF }}</p>
+      </v-card-title>
+    </v-img>
+
+    <v-card-text>
+      <div class="font-weight-bold ml-8 mb-2">Timeline</div>
+
+      <v-timeline align-top dense>
+        <v-timeline-item
+          v-for="(status, index) in this.appointmentStatusList"
+          :key="index"
+          :color="setColor(status)"
+          small
+        >
+          <div>
+            <div class="font-weight-normal">
+              <strong>{{ status.key }}</strong>
+            </div>
+          </div>
+        </v-timeline-item>
+      </v-timeline>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+import { appointmentStatus } from '@/assets/js/enums/appointmentEnum'
+import objectHelper from '../../assets/js/healpers/objectHelper'
+export default {
+  name: 'Appointment',
+  mixins: [objectHelper],
+  props: {
+    appointment: { type: Object, required: true, default: () => undefined }
+  },
+  data: () => ({
+    appointmentStatusList: []
+  }),
+  methods: {
+    createAppointmentStatusList() {
+      for (const [key, value] of Object.entries(appointmentStatus)) {
+        this.appointmentStatusList.push({ key, value })
+      }
+    },
+    setColor(status) {
+      return status.value === this.appointment.status ? 'green' : '#DAA520'
+    }
+  },
+  computed: {},
+  created() {
+    this.createAppointmentStatusList()
+  }
+}
+</script>
+<style lang="scss" scoped>
+.appointment {
+  position: relative;
+  bottom: 0;
+  transition: all ease 0.5s !important;
+}
+.appointment:hover {
+  bottom: 10px;
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2) !important;
+}
+.pointer {
+  cursor: pointer;
+}
+</style>
